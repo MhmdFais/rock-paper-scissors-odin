abf0d2c5dc2163cd63d1bbc6f7e1b7326072bbe4
Muhammed <117585798+MhmdFais@users.noreply.github.com>
2023-12-29 18:37:05 +0530

    basic winng and loosing scenarios

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let selctionArray = ['rock', 'paper', 'scissor'];
+let selctionArray = ["rock", "paper", "scissor"];
 
 function getComputerSelection(selctionArray){
     //getting the random index
@@ -13,7 +13,17 @@ function getComputerSelection(selctionArray){
 }
 
 function getPlayerSelection(){
-    
+    let userInput = prompt("Please enter your selection: ");
+    console.log(userInput);
+    return userInput;  
+}
+
+function game(){
+    let plaerWon = 0;
+    let computerWon = 0;
+    for ( let i=0; i<5 ; i++){
+        playRound(playerSelection, computerSelection);
+    }
 }
 
 
@@ -21,16 +31,36 @@ function playRound(playerSelection, computerSelection){
     let playerSelectedInCapital = playerSelection.toUpperCase();
     let computerSelectedInCapital = computerSelection.toUpperCase();
 
-
-}
-
-function game(){
-    for ( let i=0; i<5 ; i++){
-        playRound(playerSelection, computerSelection);
+    if ( playerSelectedInCapital === computerSelectedInCapital ){
+        //re play 
+    }
+    else if ( playerSelectedInCapital === "ROCK" ){
+        if ( computerSelectedInCapital === "PAPER" ){
+            return 'You lose! paper beats rock';
+        }
+        else {
+            return 'You won! rock beats scissor';
+        }
+    }
+    else if ( playerSelectedInCapital === "PAPER" ){
+        if ( computerSelectedInCapital === "ROCK" ){
+            return 'You won! paper beats rock';
+        }
+        else {
+            return 'You lose! scissor beats paper';
+        }
+    }
+    else if ( playerSelectedInCapital === "SCISSOR" ){
+        if ( computerSelectedInCapital === "ROCK" ){
+            return 'You lose! rock beats scissor';
+        }
+        else {
+            return 'You won! scissor beats rock';
+        }
     }
 }
 
-
-let computerSelection = getComputerSelection();
+let computerSelection = getComputerSelection(selctionArray);
 let playerSelection = getPlayerSelection();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));   
+
